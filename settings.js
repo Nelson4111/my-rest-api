94f5a412f0c8a737a28e390c45515cd8def261c0
Nelson4111 <101699549+Nelson4111@users.noreply.github.com>
2024-06-17 13:28:37 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
 global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.MONGO_DB_URI = "mongodb+srv://nelsonrandanan:SIYhiB7Q1Vr0U9Lc@cluster0.cpinozn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "nelsonrandanan@gmail.com" //email
+global.email_password = "vskm nkwa grop cvak" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
